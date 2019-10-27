2509885cc553a540c77250ccd1c137059b713179
Viktor Lukovets <nuerio63@gmail.com>
2019-10-27 14:23:47 +0200

    Updates

---
+++ precache-manifest.9ac7cf729ad8969382ef6af6a37ecb4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996657c00190e21b20e15a8ba7b136a0",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "be5b71fb76ba39fbcfd2",
+    "url": "/react_static-list-of-todos/static/css/main.47ed1399.chunk.css"
+  },
+  {
+    "revision": "4f3fb121ce3d95cec5e6",
+    "url": "/react_static-list-of-todos/static/js/2.463f5126.chunk.js"
+  },
+  {
+    "revision": "be5b71fb76ba39fbcfd2",
+    "url": "/react_static-list-of-todos/static/js/main.a9031e69.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_static-list-of-todos/precache-manifest.1460eb5f6aa52629add7e99c8f3a4e44.js"
+  "/react_static-list-of-todos/precache-manifest.9ac7cf729ad8969382ef6af6a37ecb4a.js"
 );
 
 self.addEventListener('message', (event) => {
